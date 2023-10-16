07a7e1696fd63ed5ab805baa6f356135ca4faa2a
a.kashina <a.kashina@websecret.by>
2023-10-16 19:44:37 +0200

    fix: update work experience, certificates

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,45 @@
-let gulp = require("gulp");
-let pug = require("gulp-pug");
-let sass = require("gulp-sass");
-let postcss = require("gulp-postcss");
-let browserSync = require("browser-sync").create();
+const gulp = require("gulp");
+const pug = require("gulp-pug");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const browserSync = require("browser-sync").create();
 
 gulp.task("build:pug", () => {
-  return gulp
-    .src("src/*.pug")
-    .pipe(pug())
-    .pipe(gulp.dest("build"));
+    return gulp.src("src/*.pug").pipe(pug()).pipe(gulp.dest("build"));
 });
 
 gulp.task("build:scss", () => {
-  return gulp
-    .src("src/sass/*.sass")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(postcss())
-    .pipe(gulp.dest("build/css"));
+    return gulp
+        .src("src/scss/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(postcss())
+        .pipe(gulp.dest("build/css"));
 });
 
 gulp.task("build:images", () => {
-  return gulp
-    .src("src/img/**/*", {
-      dot: true,
-      allowEmpty: true
-    })
-    .pipe(gulp.dest("build/img"));
+    return gulp
+        .src("src/img/**/*", {
+            dot: true,
+            allowEmpty: true,
+        })
+        .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("build", gulp.parallel("build:pug", "build:scss", "build:images"));
 
 gulp.task("serve", () => {
-  browserSync.init({
-    server: {
-      baseDir: "build"
-    }
-  });
+    browserSync.init({
+        server: {
+            baseDir: "build",
+        },
+    });
 });
 
 gulp.task("watch", () => {
-  gulp.watch("src/**/*.pug", gulp.series("build:pug"));
-  gulp.watch("src/sass/**/*.sass", gulp.series("build:scss"));
-  gulp.watch("src/img/**/*", gulp.series("build:images"));
-  gulp.watch("build/**/*").on("change", browserSync.reload);
+    gulp.watch("src/**/*.pug", gulp.series("build:pug"));
+    gulp.watch("src/scss/**/*.scss", gulp.series("build:scss"));
+    gulp.watch("src/img/**/*", gulp.series("build:images"));
+    gulp.watch("build/**/*").on("change", browserSync.reload);
 });
 
 gulp.task("default", gulp.series("build", gulp.parallel("serve", "watch")));
